85fe5acc8d1278e7b7b38b322f312d11ed6d8e2f
BrianChu <BrianChu13321@gmail.com>
2015-11-23 22:00:54 -0800

    Attempted TDD

--- tdd/spec/tddSpec.js
+++ tdd/spec/tddSpec.js
@@ -4,3 +4,13 @@ describe("New Robot", function(){
     expect(testBot.name).toEqual("TestRobot");
   })
 })
+
+// describe("Check Canvas", function(){
+//   it("Check if canvas exists", function() {
+//     var testBot = new Robot("TestRobot")
+//     expect(testBot.add(2,2)).toEqual(console.log(4));
+//   })
+// })
+
+// Attempted to test canvas
+
