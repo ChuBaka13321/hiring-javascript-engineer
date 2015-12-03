effbe22466c809ced1886cb439d10449a9104081
BrianChu <BrianChu13321@gmail.com>
2015-12-02 17:04:32 -0800

    Optimized fib.js, documented both fib.js and memoize.js.

--- performance/fib.js
+++ performance/fib.js
@@ -1,7 +1,28 @@
+// var fib = function (n) {
+//         if (n < 2) {
+//             return n;
+//         }
+//         return fib(n - 1) + fib(n - 2);
+        
+//         //recursion, fib(5) --> return fib(4) + fib(3) --> then 4 and 3 would split into 3 & 2, and 2 & 1 respectively until the base case (n < 2) is reached. Recursion is fun, but not always the fastest implementation.
+//     };
+// module.exports = fib;
+
+//reimplementation attempt
 var fib = function (n) {
-        if (n < 2) {
-            return n;
+        var result;
+        var smallIncrement = 1;
+        var largeIncrement = 1;
+        var i = 2;
+        if (n <= 2) {
+            return 1;
         }
-        return fib(n - 1) + fib(n - 2);
+        while(i < n) {
+          result = largeIncrement + smallIncrement;
+          smallIncrement = largeIncrement;
+          largeIncrement = result;
+          i++;
+        }
+        return result;
     };
 module.exports = fib;
--- performance/memoize.js
+++ performance/memoize.js
@@ -1,6 +1,12 @@
 var results = {};
+// The object where the key/values are stored after being run to save on memory. Instead of running the fib recursion function again, ideally we want to store the values, so if we've already done fibMemoized(40) once. We bypass redoing the function and just access it via the key/value from results object and return that.
+
 module.exports = function (fn) {
+    //fn represents the function fib() being passed in as a parameter.
+
     return function (param) {
+        //function here has access to "results" variable outside of its scope and is able to return the value or create new key/value pair.
+        //param in this case is "40" or the numbers being passed into fibMemoized(param);
         return results[param] || (results[param] = fn.call(this, param));
     };
 };
