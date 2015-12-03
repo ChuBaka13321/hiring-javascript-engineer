48a68b4612e9b88be35259f689c28202b2ba18ce
BrianChu <BrianChu13321@gmail.com>
2015-12-03 12:23:16 -0800

    memoize optimized for multiple parameters

--- performance/index.js
+++ performance/index.js
@@ -8,16 +8,35 @@ var fib = require('./fib'),
         console.log('----------------');
     };
 
+//just to test multiple parameters
+var sumAll = function(){
+    var sum = 0;
+    for(var i = 0; i < arguments.length; i++) {
+        sum += arguments[i];
+    }
+    return sum
+}
+
+var sumMemoized = memoize(sumAll);
+
 benchmark('fib(40)', function () {
     return fib(40);
 });
 
 benchmark('1st fibMemoized(40) execution', function () {
-    return fibMemoized(40);
+    return fibMemoized(10, 10000, 40);
 });
 
 benchmark('2nd fibMemoized(40) execution', function () {
-    return fibMemoized(40);
+    return fibMemoized(10000, 10000, 40);
+});
+
+benchmark('1st sumMemoized execution', function () {
+    return sumMemoized(1, 3, 5);
+});
+
+benchmark('2nd sumMemoized execution', function () {
+    return sumMemoized(1, 3, 5);
 });
 
 /*
--- performance/memoize.js
+++ performance/memoize.js
@@ -1,12 +1,39 @@
+// var results = {};
+// // The object where the key/values are stored after being run to save on memory. Instead of running the fib recursion function again, ideally we want to store the values, so if we've already done fibMemoized(40) once. We bypass redoing the function and just access it via the key/value from results object and return that.
+
+// module.exports = function (fn) {
+//     //fn represents the function fib() being passed in as a parameter to memoize(fn).
+
+//     return function (param) {
+//         //function here has access to "results" variable outside of its scope and is able to return the value or create new key/value pair.
+//         //param in this case is "40" or the numbers being passed into fibMemoized(param);
+//         return results[param] || (results[param] = fn.call(this, param));
+//         //returns results[param] if exists, otherwise results[param] is now set to the result of fn() being called on the parameter being passed in, and is then returned.
+//     };
+// };
+
+
+// Multiple parameters, like, lets suppose you want to change fib to sumAll, and sumAll accepts N params, sumAll(1,2,3) = 6, sumAll(5,1,0) = 6 too.
+//note: try sorting params as they come in to get the same order. ex: (4,5) & (5,4) --> (4,5) & (4,5)
+
 var results = {};
 // The object where the key/values are stored after being run to save on memory. Instead of running the fib recursion function again, ideally we want to store the values, so if we've already done fibMemoized(40) once. We bypass redoing the function and just access it via the key/value from results object and return that.
 
 module.exports = function (fn) {
-    //fn represents the function fib() being passed in as a parameter.
+    //fn represents the function fib() being passed in as a parameter to memoize(fn).
 
-    return function (param) {
-        //function here has access to "results" variable outside of its scope and is able to return the value or create new key/value pair.
-        //param in this case is "40" or the numbers being passed into fibMemoized(param);
-        return results[param] || (results[param] = fn.call(this, param));
+    return function () {
+        var argArray = [];
+        for (var i = 0; i < arguments.length; i++) {
+          argArray.push(arguments[i])
+        };
+        argArray.sort()
+        if(!(argArray.toString() in results)) {
+          results[argArray.toString()] = fn.apply(this, argArray)
+        }
+        return results[argArray.toString()]
+
+        // return results[param] || (results[param] = fn.apply(this, arguments));
+
     };
 };
