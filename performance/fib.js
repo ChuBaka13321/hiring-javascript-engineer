d1d1f685cd0cd703493f7c156fb388317df0369a
BrianChu <BrianChu13321@gmail.com>
2015-12-04 14:13:16 -0800

    reimplementation of fib.js with recursion + cache

--- performance/fib.js
+++ performance/fib.js
@@ -3,26 +3,60 @@
 //             return n;
 //         }
 //         return fib(n - 1) + fib(n - 2);
-        
-//         //recursion, fib(5) --> return fib(4) + fib(3) --> then 4 and 3 would split into 3 & 2, and 2 & 1 respectively until the base case (n < 2) is reached. Recursion is fun, but not always the fastest implementation.
 //     };
 // module.exports = fib;
 
-//reimplementation attempt
-var fib = function (n) {
-        var result;
-        var smallIncrement = 1;
-        var largeIncrement = 1;
-        var i = 2;
-        if (n <= 2) {
-            return 1;
-        }
-        while(i < n) {
-          result = largeIncrement + smallIncrement;
-          smallIncrement = largeIncrement;
-          largeIncrement = result;
-          i++;
-        }
-        return result;
-    };
+// var fib = function (n) {
+//   //cache up here
+//   //subroutine function here
+//         if (n < 2) {
+//             return n;
+//         }
+//         return fib(n - 1) + fib(n - 2);
+//     };
+
+//2nd reimplementation attempt with cache and recursion
+var fib = function(x) {
+  var fibCache = {};
+  //Object where the key/values are stored and accessed to save on time/efficiency
+  var subRoutine = function(n, cache) {
+    if(!cache[n]) {
+    //check if cache[n] exists, if it doesn't exist, proceed with following
+      if (n < 2) {
+        cache[n] = n;
+        //base case of 0 or 1;
+      } else {
+        cache[n] = subRoutine(n-1, cache) + subRoutine(n-2, cache);
+        //numbers 2 & greater get defined here, with subsequent recursive calls. each subRoutine returns their value after each recursion reaches base case.
+      }
+    }
+    return cache[n];
+    //return the final value/total here
+  };
+  return subRoutine(x, fibCache)  
+  //pass fibCache object as secondary parameter to save on execution of subRoutine
+}
+
+
 module.exports = fib;
+
+// //reimplementation attempt
+// var fib = function (n) {
+//         var result;
+//         var smallIncrement = 1;
+//         var largeIncrement = 1;
+//         var i = 2;
+//         if (n <= 2) {
+//             return n;
+//         }
+//         while(i < n) {
+//           result = largeIncrement + smallIncrement;
+//           smallIncrement = largeIncrement;
+//           largeIncrement = result;
+//           i++;
+//         }
+//         return result;
+//     };
+// module.exports = fib;
+
+
--- performance/index.js
+++ performance/index.js
@@ -15,29 +15,43 @@ var sumAll = function(){
         sum += arguments[i];
     }
     return sum
-}
+};
 
 var sumMemoized = memoize(sumAll);
 
+var paramsTest = function (a,b,c,d,e) {
+        return [a,b,c,d,e].join(' - ');
+    },
+    paramsTestMemo = memoize(paramsTest);
+
+benchmark('paramsTest: 5, 4, 3, 2, 1', function () {
+    return paramsTest(5, 4, 3, 2, 1);
+});
+
+benchmark('paramsTestMemo: 5, 4, 3, 2, 1', function () {
+    return paramsTestMemo(5, 4, 3, 2, 1);
+});
+
+
 benchmark('fib(40)', function () {
-    return fib(40);
+    return fib(7);
 });
 
-benchmark('1st fibMemoized(1000) execution', function () {
-    return fibMemoized(1000);
-});
+// benchmark('1st fibMemoized(1000) execution', function () {
+//     return fibMemoized(40000000);
+// });
 
-benchmark('2nd fibMemoized(1000) execution', function () {
-    return fibMemoized(1000);
-});
+// benchmark('2nd fibMemoized(1000) execution', function () {
+//     return fibMemoized(40);
+// });
 
-benchmark('1st sumMemoized execution', function () {
-    return sumMemoized(1, 3, 5);
-});
+// benchmark('1st sumMemoized execution', function () {
+//     return sumMemoized(1, 3, 5);
+// });
 
-benchmark('2nd sumMemoized execution', function () {
-    return sumMemoized(1, 3, 5);
-});
+// benchmark('2nd sumMemoized execution', function () {
+//     return sumMemoized(1, 3, 5);
+// });
 
 /*
 // Warning:
